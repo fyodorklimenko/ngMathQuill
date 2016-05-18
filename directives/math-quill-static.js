a8543f2b04574e1065e35ab045138ed4bfa1d3dd
Fedir Klymenko <klimenko.fyodor@gmail.com>
2016-05-18 23:57:47 +0300

    init

---
+++ directives/math-quill-static.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    'use strict';
+    angular.module('app')
+        .directive('mathQuillStatic', function () {
+            return {
+                restrict: 'A',
+                scope: {
+                    initialValue: '@',
+                },
+                link: function (scope, element) {
+                    var MQ = MathQuill.getInterface(2);
+                    var element = $(element);
+                    element.html(scope.initialValue);
+                    MQ.StaticMath($(element)[0]);
+                }
+            }
+        })
+})();
