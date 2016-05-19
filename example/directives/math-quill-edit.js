c983b5bb6586b564780da96fb4562c76a482e608
Fedir Klymenko <klimenko.fyodor@gmail.com>
2016-05-19 22:53:23 +0300

    add mgCtrl's methods into link

---
+++ example/directives/math-quill-edit.js
@@ -0,0 +1,44 @@
+﻿(function () {
+    'use strict';
+    angular.module('app')
+        .directive('mathQuillEdit', function () {
+            return {
+                restrict: 'A',
+                require: 'ngModel',
+                scope: {
+                    value: '=',
+                },
+                link: function (scope, element, attrs, ngModelCtrl) {
+                    var vm = scope;
+                    console.log(vm);
+                    console.log(ngModelCtrl )
+
+                    ngModelCtrl.$formatters.push(function(modelValue) {
+                        return modelValue;
+                    });
+
+                    ngModelCtrl.$parsers.push(function(viewValue) {
+                        return viewValue;
+                    });
+
+                    vm.$watch(function() { return vm.value; }, function(nv, ov) {
+                        ngModelCtrl.$setViewValue(nv);
+                    });
+
+                    
+
+                    var MQ = MathQuill.getInterface(2);
+                    var mq = MQ.MathField($(element)[0], {
+                        handlers: {
+                            edit: function () {
+                                if (mq) {
+                                    vm.value = mq.latex();
+                                    ngModelCtrl.$setViewValue(mq.latex());
+                                }
+                            },
+                        },
+                    });
+                },
+            }
+        });
+})();
